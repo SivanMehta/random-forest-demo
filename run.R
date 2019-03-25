c43375fc282e80c67aa3b5e71b2095961a1a8793
Sivan Mehta <smehta@godaddy.com>
2019-03-24 22:26:15 -0700

    it works

--- run.R
+++ run.R
@@ -5,17 +5,24 @@ library(ggplot2)
 # generate dataset first
 # inside band is one group
 # outside band is another group
+
 f <- function(x) {
   2 * sin(x)
 }
-
 n = 10000
-group.1 <- tibble(x = runif(n) * 20, y = f(x) + runif(n, -1, 1), group = '1') %>%
+
+group.1 <- tibble(x = runif(n) * 20, y = f(x) + runif(n, -1, 1), group = 0) %>%
   filter(abs(y - f(x)) > .5)
+group.2 <- tibble(x = runif(n/2) * 20, y = f(x) + runif(n/2, -.4, .4), group = 1)
 
-group.2 <- tibble(x = runif(n/2) * 20, y = f(x) + runif(n/2, -.4, .4), group = '2')
-data <- bind_rows(group.1, group.2)
+data <- bind_rows(group.1, group.2) # bind groups together
+data <- data[sample(1:nrow(data)), ] # shuffle rows
 
-ggplot(data) +
-  geom_point(aes(x, y, color = group))
+total = nrow(data)
+train <- data[1:(nrow(data)/2),]
+test <- data[(nrow(data)/2):nrow(data),]
+rf <- randomForest(group ~ ., train)
 
+results <- ifelse(predict(rf, newdata = test) > .5, 1, 0)
+accuracy <- length(which(results == test$group)) / nrow(test)
+print(accuracy * 100)
