62451f1cb63b76343f58751c45af3366a3359cf4
Sivan Mehta <smehta@godaddy.com>
2019-03-24 22:48:01 -0700

    [ux] add accuracy to plot

--- run.R
+++ run.R
@@ -11,9 +11,9 @@ f <- function(x) {
 }
 n = 10000
 
-group.1 <- tibble(x = runif(n) * 20, y = f(x) + runif(n, -1, 1), group = 0) %>%
+group.1 <- tibble(x = runif(n) * 10, y = f(x) + runif(n, -1, 1), group = 0) %>%
   filter(abs(y - f(x)) > .5)
-group.2 <- tibble(x = runif(n/2) * 20, y = f(x) + runif(n/2, -.4, .4), group = 1)
+group.2 <- tibble(x = runif(n/2) * 10, y = f(x) + runif(n/2, -.4, .4), group = 1)
 
 data <- bind_rows(group.1, group.2) # bind groups together
 data <- data[sample(1:nrow(data)), ] # shuffle rows
@@ -23,11 +23,19 @@ train <- data[1:(nrow(data)/2),]
 test <- data[(nrow(data)/2):nrow(data),]
 rf <- randomForest(group ~ ., train)
 
-test %>%
-  mutate(group.hat = ifelse(predict(rf, newdata = tibble(x, y)) > .5, 1, 0)) %>%
+predictions <- test %>%
+  mutate(group.hat = ifelse(predict(rf, newdata = tibble(x, y)) > .5, 1, 0))
+
+accuracy <- round(length(which(predictions$group == predictions$group.hat)) / nrow(predictions) * 10000)
+title <- paste(
+  'Grey are correctly classified, red was wrong. Overall accuracy: ', accuracy / 100, '%',
+  sep = ''
+)
+
+predictions %>%
   mutate(color = ifelse(group == group.hat, group, 3)) %>%
   mutate(color = as.factor(color)) %>%
   ggplot() +
     geom_point(aes(x, y, color = color, alpha = color)) +
     scale_colour_manual(values = c("#444444", "#777777", "#ff0000")) +
-    labs(title = 'Grey are correctly classified, red was wrong')
+    labs(title = title)
